de8208e3e8a907760b4af20d18ca9250fd4ae4f7
KazakovaNet <kazakovaNet@yandex.ru>
2020-01-03 19:48:51 +0300

    Update gradle and libraries versions

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,10 +1,10 @@
 package com.example.android.miwok;
 
 import android.content.Context;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 
 class CategoryAdapter extends FragmentPagerAdapter {
     /**
